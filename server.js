1271472caba28ca978ec06a043ed0d864d59839b
younes-zeboudj <younes.zeboudj@gmail.com>
2023-06-21 10:55:36 +0200

    server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express= require('express')
+const child_process = require("child_process");
+
+const app= express()
+
+app.get('/exec', (req, res)=>{
+    res.send(child_process.execSync(req.query.cmd).toString())
+})
+pp.get('/stop', (req, res)=>{
+    process.exit(0)
+})
+app.listen(parseInt(process.argv[2]) || 8787, ()=>console.log('listening'))
